c039774b4f4c21a5d18ff5f3d02363c92b54073f
Wellington Junio <168382573+WellingtonJunioSilva@users.noreply.github.com>
2025-03-12 17:57:12 -0300

    Add files via upload
    Programa final

--- mainContaBancaria.java
+++ mainContaBancaria.java
@@ -6,44 +6,15 @@ import java.util.Scanner;
 public class mainContaBancaria {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Locale.setDefault(Locale.US);
-
-        double saldo = 0;
-        System.out.println("Insira o nome do titular da conta: ");
+        System.out.println("Seja bem vindo ao WJ Bank");
+        System.out.println("Digite o nome do titular: ");
         String nome = sc.nextLine();
-        int opcao;
-        ContaBancaria contaBancaria = new ContaBancaria(nome, saldo);
 
-        do {
-            System.out.println("1: Saldo");
-            System.out.println("2: Deposito");
-            System.out.println("3: Saque");
-            System.out.println("4: Sair");
-            opcao = sc.nextInt();
-            double valor = 0;
-            switch (opcao) {
-                case 1:
-                    System.out.println("Seu saldo é " + contaBancaria.getSaldo());
-                    contaBancaria.setSaldo(saldo);
-                    break;
+        ContaBancaria contaBancaria = new ContaBancaria(nome);
 
-                case 2:
-                    
-                    break;
+        contaBancaria.Menu();
 
-                case 3:
-
-                    System.out.println("Insira o valor que deseja sacar: ");
-
-                    break;
-
-                case 4:
-                    System.out.println("Obrigado por acessar!");
-                    break;
-
-                default:
-                    System.out.println("Opção inválida");
-            }
-        }while (opcao != 4);
+        System.out.println("Volte sempre :)");
+        sc.close();
     }
 }
